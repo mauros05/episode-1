52ee419b642102bd2ae1745e4207b2509bae3cf8
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-10-24 21:09:17 -0600

    Check css and conditionals

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 
 function App() {
-  return (
-  <div className="App">
-    <User name="Mauricio" age={25} email="mauricio.gutley@gmail.com" />
-  </div>
-  );
+
+  const age = 14
+
+  return <div className="App">{ age >= 18 ? <h1>Puedes Votar!</h1> : <h1>No puedes Votar!</h1> }</div>
+
+  // return (
+  // <div className="App">
+  //   <User name="Mauricio" age={25} email="mauricio.gutley@gmail.com" />
+  //   <User name="Concepcion" age={57} email="conchitamol26@hotmail.com" />
+  // </div>
+  // );
 }
 
 const User = (props) => {
   return (
   <div>
-    <h1>{props.name}</h1>
+    <h1 className="name">{props.name}</h1>
     <h1>{props.age}</h1>
     <h1>{props.email}</h1>
   </div>
