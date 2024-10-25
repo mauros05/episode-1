5e9d40930931aca713ba8cf277b2c3d3d3c6b0d8
Mauricio Gutierrez <mauricio.gutley@gmail.com>
2024-10-24 21:24:39 -0600

    More options with conditionals

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 
 function App() {
 
-  const age = 14
+  const age = 14;
+  const isGreen = false;
 
-  return <div className="App">{ age >= 18 ? <h1>Puedes Votar!</h1> : <h1>No puedes Votar!</h1> }</div>
+  return (
+    <div className="App">
+      { age >= 18 ? <h1>Puedes Votar!</h1> : <h1>No puedes Votar!</h1> }
+      <h1 style={{ color: isGreen ? "green" : "red" }}>This is a color</h1>
+
+      {isGreen && <button>This is a Button</button>}
+    </div>
+  );
 
   // return (
   // <div className="App">
